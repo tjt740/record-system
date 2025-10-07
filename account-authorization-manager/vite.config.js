9f5b78f44d9716060fcd2b8151fdbe9af82fa777
tjt740 <15751666937@163.com>
2025-10-08 01:51:10 +0800

    Initial deploy

--- account-authorization-manager/vite.config.js
+++ account-authorization-manager/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'record-system',
+    base: 'record-system/account-authorization-manager/',
     plugins: [react()],
 });
